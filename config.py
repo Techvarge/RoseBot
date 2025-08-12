52627d7c4b145ef2e050012b7d15f31b03709ad5
Techvarge <155867245+Techvarge@users.noreply.github.com>
2025-08-12 07:05:48 +0600

    Update config.py

--- config.py
+++ config.py
@@ -11,9 +11,10 @@ API_HASH1 = environ.get("API_HASH1")
 SUDO_USERS_ID = environ.get("SUDO_USERS_ID")
 LOG_GROUP_ID = environ.get("LOG_GROUP_ID")
 BASE_DB = environ.get("BASE_DB")
-MONGO_URL = environ.get("MONGO_URL")
+MONGO_URL = environ.get("mongodb+srv://Rosebot:<Rosedb>@cluster0.oeagvnj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 ARQ_API_URL = environ.get("ARQ_API_URL")
 ARQ_API_KEY = environ.get("ARQ_API_KEY")
 COMMAND_PREFIXES = environ.get("COMMAND_PREFIXES")
 
 F_SUB_CHANNEL = environ.get("F_SUB_CHANNEL")
+
